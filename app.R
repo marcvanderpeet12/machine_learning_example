ac31027f344a27f3be1ffe5550e77566c00da12e
marcvanderpeet12 <marcvanderpeet@gmail.com>
2018-02-04 19:36:05 +0100

    'Fixed random forest functionality 'Create bullet chart to show the results

--- app.R
+++ app.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(dplyr)
 library(DT)
 library(titanic)
 library(randomForest)
@@ -6,13 +7,11 @@ library(randomForest)
 ui <- fluidPage(
   
   DT::dataTableOutput("mytable"),
-  checkboxInput("checkbox" , label = "age", value = FALSE),
-  checkboxInput("checkbox" , label = "class", value = FALSE),
-  checkboxInput("checkbox" , label = "station", value = FALSE),
+  checkboxGroupInput("myselection", "select columns", choices = c("Pclass", "Sex"), inline = TRUE),
   actionButton("runRF", "Predict"),
-  plotOutput("plotRF")
-)
+  plotOutput("outcome")
 
+)
 server <- function(input, output, session) {
   
   output$mytable = DT::renderDataTable({
@@ -22,15 +21,30 @@ server <- function(input, output, session) {
   
   observeEvent(input$runRF, {
  
-    #This should be filled with input, not working yet
-    var = c("Pclass")
-     
-    fit <- randomForest(as.factor(Survived) ~ Pcalss, data = titanic_train, importance = TRUE, ntree=2000)
-    prediction <- as.numeric(predict(fit, titanic_test))
-    titanic_test$predicted <- prediction  
+    titanic_train <- titanic_train[complete.cases(titanic_train),]
+    titanic_train$Sex <- as.factor(titanic_train$Sex)
     
-    output$plotRF <- renderPlot({
-      hist(prediction)
+    sample <- sample.int(n = nrow(titanic_train), size = floor(.75*nrow(titanic_train)), replace = F)
+    t_train <- titanic_train[sample,]
+    t_test <- titanic_train[-sample,]
+    
+    fit <- randomForest(x = t_train[, "Sex", drop = FALSE],
+     y = as.factor(t_train$Survived), importance = TRUE, ntree = 2000)
+    prediction <- as.numeric(predict(fit, t_test))
+    t_test$predicted <- prediction  
+    t_test$correct <- as.numeric(ifelse(t_test$predicted == t_test$Survived, 1, 0))
+    perc <- sum(t_test$correct)/nrow(t_test)
+    
+    df_graph = data.frame(id = "perc", acc = perc, n = 1)
+    
+    output$outcome <- renderPlot({
+      as.character(perc)
+      
+      ggplot(df_graph) +
+        geom_bar(aes(id, n), fill ="goldenrod2", stat ="identity", width = 0.5, alpha = 0.2) +
+        geom_bar(aes(id, acc), fill ="black", stat ="identity", width = 0.2, alpha = 0.2) +
+        ggtitle(paste0("The accuracy using this algorithm is: ", perc, " for ", nrow(t_test), "observations."))
+        
     })
   })
 }
